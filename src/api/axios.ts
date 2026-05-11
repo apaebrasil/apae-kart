19a01f5da7004f1885321ee5e6daad7ab0a70ebe
felipe enzou <felipe.enzou@gmail.com>
2026-05-11 10:48:26 -0300

    fix: ajuste

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-  baseURL: "/api/fluig-proxy.php",
+  baseURL: "https://maroon-gnu-600298.hostingersite.com",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
