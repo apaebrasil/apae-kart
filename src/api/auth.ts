d63e314c47a69b17dae8c9ad72f3a6022952ea9a
felipe enzou <felipe.enzou@gmail.com>
2026-05-11 10:40:57 -0300

    fix: ajuste

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,7 @@ import CryptoJs from "crypto-js";
 import axios, { type AxiosRequestHeaders, type InternalAxiosRequestConfig } from "axios";
 
 // URL do Fluig (usada para assinar requisições OAuth em desenvolvimento)
-const FLUIG_BASE_URL = "https://federacaonacional201538.fluig.cloudtotvs.com.br/fluig/";
+const FLUIG_BASE_URL = "https://federacaonacional201538.fluig.cloudtotvs.com.br";
 
 // Determina a base URL dependendo do ambiente
 const getBaseURL = () => {
