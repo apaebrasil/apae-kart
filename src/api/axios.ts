ef9a652651fcc94a507be40838ad3fc26708eac6
felipe enzou <felipe.enzou@gmail.com>
2026-05-11 09:48:48 -0300

    fix: ajuste da base url para produção

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-  baseURL: "/",
+  baseURL: "https://maroon-gnu-600298.hostingersite.com/",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
